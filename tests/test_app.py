4bccd007063b697396387f73838ab601857b622b
DandanLeinad <danieldias6788@gmail.com>
2025-05-10 02:07:29 -0300

    fix: melhora a documentação do teste de leitura com descrição das etapas AAA

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,16 @@ from fastapi_zero.app import app
 
 
 def test_read_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA):
+    - A: Arrange: Arranjo
+    - A: Act: Executa a coisa (o SUT)
+    - A: Assert: Garante que A é A
+    """
+
+    # Arrange
     client = TestClient(app)
+    # Act
     response = client.get("/")
+    # Assert
     assert response.json() == {"message": "Olá, Mundo!"}
