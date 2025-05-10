c0be1264272dce529f193440c3ba8d03ad8097e5
DandanLeinad <danieldias6788@gmail.com>
2025-05-10 02:00:58 -0300

    fix: corrige mensagem de retorno da API e organiza teste de leitura

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,4 +2,8 @@ from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
-client = TestClient(app)
+
+def test_read_deve_retornar_ola_mundo():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.json() == {"message": "Olá, Mundo!"}
