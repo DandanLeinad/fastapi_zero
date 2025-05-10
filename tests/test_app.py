28d374978523cc5de3148adca456c18bfbdaa325
DandanLeinad <danieldias6788@gmail.com>
2025-05-10 02:08:00 -0300

    fix: corrige formatação da documentação do teste de leitura

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,9 @@ from fastapi_zero.app import app
 def test_read_deve_retornar_ola_mundo():
     """
     Esse teste tem 3 etapas (AAA):
-    - A: Arrange: Arranjo
-    - A: Act: Executa a coisa (o SUT)
-    - A: Assert: Garante que A é A
+    - A: Arrange - Arranjo
+    - A: Act - Executa a coisa (o SUT)
+    - A: Assert - Garante que A é A
     """
 
     # Arrange
