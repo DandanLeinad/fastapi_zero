3a5d64d6a4348952d270c54a22be88c874624c72
DandanLeinad <danieldias6788@gmail.com>
2025-05-13 21:09:40 -0300

    fix: altera o endpoint raiz para retornar HTML em vez de JSON

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,10 +1,17 @@
-from http import HTTPStatus
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/", status_code=HTTPStatus.OK)
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "Olá, Mundo!"}
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
