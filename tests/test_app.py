84e3bcf3ca8fee2d1c6b51b651eb83a8ecd20533
DandanLeinad <danieldias6788@gmail.com>
2025-05-18 01:53:21 -0300

    test: adiciona teste para atualização de usuários

--- tests/test_app.py
+++ tests/test_app.py
@@ -46,3 +46,21 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "secret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "id": 1,
+        "username": "bob",
+        "email": "bob@example.com",
+    }
