0c7681c6cf71abae409cb4d7a28903423a4fa9ef
DandanLeinad <danieldias6788@gmail.com>
2025-05-18 01:01:31 -0300

    feat: adiciona fixture de cliente para testes e refatora testes existentes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_read_deve_retornar_ola_mundo():
+def test_read_deve_retornar_ola_mundo(client):
     """
     Esse teste tem 3 etapas (AAA):
     - A: Arrange - Arranjo
@@ -13,18 +9,13 @@ def test_read_deve_retornar_ola_mundo():
     - A: Assert - Garante que A é A
     """
 
-    # Arrange
-    client = TestClient(app)
-    # Act
     response = client.get("/")
-    # Assert
+
     assert response.json() == {"message": "Olá, Mundo!"}
     assert response.status_code == HTTPStatus.OK
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         "/users/",
         json={
