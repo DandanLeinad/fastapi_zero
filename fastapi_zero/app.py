6cb29aa4b59f966cfd2608d36537f1b0b2645e68
DandanLeinad <danieldias6788@gmail.com>
2025-05-18 00:43:54 -0300

    feat: simplifica a criação de usuários utilizando model_dump

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -15,11 +15,6 @@ def read_root():
 
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        id=len(database) + 1,
-        username=user.username,
-        email=user.email,
-        password=user.password,
-    )
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
     database.append(user_with_id)
     return user_with_id
