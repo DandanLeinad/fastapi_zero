8050221a237d773b6884ee01ad99e131b41afb98
DandanLeinad <danieldias6788@gmail.com>
2025-05-18 00:13:29 -0300

    feat: adiciona endpoint para criação de usuários

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserSchema
 
 app = FastAPI(title="FastAPI Zero")
 
@@ -10,3 +10,8 @@ app = FastAPI(title="FastAPI Zero")
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá Mundo!"}
+
+
+@app.post("/users/")
+def create_user(user: UserSchema):
+    return user
