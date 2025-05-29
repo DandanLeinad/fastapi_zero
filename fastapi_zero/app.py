85d5d741c90eaa84c606afc352800547db180531
Daniel Silva <danieldias6788@gmail.com>
2025-05-29 20:53:49 -0300

    feat: adiciona título à instância do FastAPI

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi_zero.routers import auth, users
 from fastapi_zero.schemas import Message
 
-app = FastAPI()
+app = FastAPI(title="FastAPI Zero")
 
 app.include_router(users.router)
 app.include_router(auth.router)
