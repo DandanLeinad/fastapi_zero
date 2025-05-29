9fafcadbdf84c421c07aeef75eab3137a0a01bfd
Daniel Silva <danieldias6788@gmail.com>
2025-05-29 20:35:08 -0300

    fix: atualiza rota para obtenção de token de autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -172,7 +172,7 @@ def test_create_user_email_conflict(client, user):
 
 def test_get_token(client, user):
     response = client.post(
-        "/token",
+        "/auth/token",
         data={
             "username": user.email,
             "password": user.clean_password,
