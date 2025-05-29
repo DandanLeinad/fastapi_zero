79ec9f3f55cd2bd53ea9d3eba1634792dd0e7365
Daniel Silva <danieldias6788@gmail.com>
2025-05-29 20:23:17 -0300

    feat: adiciona roteador de autenticação e implementa login para geração de token

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -30,25 +30,4 @@ def read_root():
     return {"message": "Olá, Mundo!"}
 
 
-@app.post("/token", response_model=Token)
-def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
-    user = session.scalar(select(User).where(User.email == form_data.username))
 
-    if not user:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail="Incorrect email or password",
-        )
-
-    if not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail="Incorrect email or password",
-        )
-
-    access_token = create_access_token(data={"sub": user.email})
-
-    return {"access_token": access_token, "token_type": "bearer"}
